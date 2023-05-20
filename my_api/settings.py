8960138b9b5d6d2e04b25ee1bbca9ca0f8fa15f8
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-05-20 14:51:05 +0300

    Update settings.py

--- my_api/settings.py
+++ my_api/settings.py
@@ -103,6 +103,9 @@ REST_FRAMEWORK = {
        
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+   'DEFAULT_PERMISSION_CLASS': (        
+    '  rest_framework.permissions.IsAuthenticated',    
+    ),
     
    
 }
