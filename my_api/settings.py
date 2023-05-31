5f2806e69c90564b30927960acd6f1a58ff05c97
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-05-31 12:11:58 +0300

    Update settings.py

--- my_api/settings.py
+++ my_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'  
   
 # Path where media is stored  
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
