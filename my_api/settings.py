2a80cd2949d8bfaa95055d88ded070141831b02f
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-05-31 12:29:26 +0300

    Update settings.py

--- my_api/settings.py
+++ my_api/settings.py
@@ -22,11 +22,11 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = '/media/'  
+
   
 # Path where media is stored  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
-
+MEDIA_ROOT = '/home/user/photos/' 
+MEDIA_URL = '/photos/'  
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
