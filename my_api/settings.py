feb1a5a6e58cf5d45ecfb9d471a05995d312b0f9
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-06-10 12:05:39 +0300

    Update settings.py

--- my_api/settings.py
+++ my_api/settings.py
@@ -89,14 +89,15 @@ WSGI_APPLICATION = 'my_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
       
     }
-}"""
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -106,7 +107,7 @@ DATABASES = {
         'HOST': 'containers-us-west-145.railway.app',
         'PORT': '5807',
     }
-}
+}"""
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
