c635b60f8fdd3a6d5ca51b71048fb615ab23130c
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-06-15 02:34:34 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('alarm/', Alarm.as_view()),
      path('history/', history.as_view()),
     path('UserChangename/', UserChangename.as_view(), name='UserChangename'),
-    UserChangename
+
     
     
     
