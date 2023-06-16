156591e0b7efdeec734a2864a2ee7ae2d36502e2
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-06-16 16:36:38 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -150,7 +150,16 @@ class history(APIView):
            serializer =alarmSerilazer(userd, many=True)
            return Response(serializer.data)
   
+class MyModelDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MyModel.objects.all()
+    serializer_class = UserProfileSerializer
 
+    def put(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 
