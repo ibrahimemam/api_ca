446d11ce184be74ccf8b9dfcd7ec54c0ef13efd7
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-06-16 18:55:20 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path,include
-from api.views import Alarm,history,MyAPIView,UserRegistrationView,MyModelDetail,UserView,UserLoginView,UserProfileView,camiraView,UserChangePasswordView,SendPasswordResetEmailView,UserPasswordResetView,index
+from api.views import Alarm,history,ImageUploadView,MyAPIView,UserRegistrationView,MyModelDetail,UserView,UserLoginView,UserProfileView,camiraView,UserChangePasswordView,SendPasswordResetEmailView,UserPasswordResetView,index
 from django.conf import settings  
 from django.conf.urls.static import static  
 urlpatterns = [
@@ -18,7 +18,7 @@ urlpatterns = [
     path('alarm/', Alarm.as_view()),
     path('history/', history.as_view()),
     path('editProfile/', UserProfileView.as_view(), name='profile'),
-   
+    path('upload-image/', ImageUploadView.as_view(), name='upload-image'),
     path('mymodels/<int:pk>/', MyModelDetail.as_view(), name='mymodel_detail'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
