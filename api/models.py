cf5fa672a2b50d1eae498b3671e1229141958532
ibrahimemam <76545909+ibrahimemam@users.noreply.github.com>
2023-06-17 05:01:17 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -91,8 +91,7 @@ class MyModel(models.Model):
     
     image_url = models.ImageField(upload_to='my_picter', blank=True, null=True)
     alrm_at = models.DateTimeField(auto_now_add=True)
-      def __str__(self):
-        return self.alarm
+
 class Reservation(models.Model):
     camira = models.ForeignKey(camira, related_name='reservation', on_delete=models.CASCADE )
     movie = models.ForeignKey(User, related_name='reservation', on_delete=models.CASCADE )
@@ -104,6 +103,4 @@ class alarm(models.Model):
     
     image_ur = models.ImageField(upload_to='my_picter', blank=True, null=True)
     alrm_in = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.alarmat
-      
+
